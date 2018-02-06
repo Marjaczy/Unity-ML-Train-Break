d7158a4e3d3c3ec3510e355aef329e7d15d76dad
Marjaczy <32833245+Marjaczy@users.noreply.github.com>
2018-02-06 09:11:40 +0100

    Unity ML Train Break
    Got some Issues

---
+++ Zug NeuralNetwork/Assets/Skript/ForceToMove.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceToMove : MonoBehaviour
+{
+    public float acceleration = 80f; //Beschleunigun
+    private float breakForce;   //Bremskraft
+
+    private bool FreeRun = false;
+    private bool Break = false;
+
+    float[] inputs = new float[1]; //unsicher ob variable nicht in Funktion stehen muss
+
+    private bool initialized = false;
+    private Transform Stoßplatte;
+
+
+    private NeuralNetwork net;
+    private Rigidbody rBody;
+    private Material[] mats;
+
+    void Update()
+    {
+        //Run
+        //if (FreeRun == false && Break == false)
+        //{
+        //    GetComponent<Rigidbody>().AddForce(transform.right * acceleration, ForceMode.VelocityChange);
+        //    GetComponent<Rigidbody>().useGravity = true;
+        //}
+        ////Break
+        //if (Break == true)
+        //{
+        //    GetComponent<Rigidbody>().AddForce(-transform.right * breakForce, ForceMode.VelocityChange);
+        //    GetComponent<Rigidbody>().useGravity = true;
+        //    inputs[0] = breakForce;
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //If hit coliderbox "FreeRun" turn Break off
+        if (other.gameObject.CompareTag("Free Run"))
+        {
+            FreeRun = true;
+            Break = false;
+        }
+
+        //If hit coliderbox "Break" turn break on
+        if (other.gameObject.CompareTag("Break"))
+        {
+            Break = true;
+        }
+    }
+
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        mats = new Material[transform.childCount];
+        for (int i = 0; i < mats.Length; i++)
+            mats[i] = transform.GetChild(i).GetComponent<Renderer>().material;
+    }
+
+    private void FixedUpdate()
+    {
+        if (initialized == true)
+        {
+            if (FreeRun == false && Break == false)
+            {
+                GetComponent<Rigidbody>().AddForce(transform.right * acceleration, ForceMode.VelocityChange);
+                GetComponent<Rigidbody>().useGravity = true;
+            }
+            //Break
+            if (Break == true)
+            {
+                GetComponent<Rigidbody>().AddForce(-transform.right * breakForce, ForceMode.VelocityChange);
+                GetComponent<Rigidbody>().useGravity = true;
+                inputs[0] = breakForce;
+            }
+
+            //float distance = Vector3.Distance(rBody.position, Stoßplatte.position);
+            //if (distance <= 50f)
+            //{
+            //    print("angekommen");
+            //    //Fehlerbehaftet
+            //}
+
+
+
+            //Aus Update Möglicherweise übernehmen
+
+
+
+            float[] outputs = net.FeedForward(inputs);      //speichert vorhergehendes Ergebnis 
+
+            net.AddFitness((1f - Mathf.Abs(inputs[0])));    //Fitness zwischen 0 - 1 
+
+            Init(net);
+        }
+
+    }
+
+    public void Init(NeuralNetwork net) //Anstatt Stoßplatte muss hier Bremskraft eingestellt werden
+    {
+        //this.breakForce = breakForce;
+        this.net = net;
+        initialized = true;
+    }
+
+
+
+    //to do:    -Bremskraft vom System verwendet
+    //          -Bremskraft regulierbar
+    //          -Populationsize wird nicht wirklich benötigt 
+    //          -NN richtig konfigurieren
+    //          -GUI Geschwindigkeits- & Beschleunigungsmessung
+    //          -Abstand zum Vorgehenden Partner muss ebenfalls positives Ergebnis auswerfen
+    //          -Füllgrad >= 60% ???
+
+}
---
+++ Zug NeuralNetwork/Assets/Skript/ManagerNN.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerNN : MonoBehaviour
+{
+public GameObject TrainPrefab;
+public GameObject Stoßplatte;
+
+public int Interval = 5;
+
+
+
+private bool isTraning = false;
+private int populationSize = 20;
+private int generationNumber = 0;
+private int[] layers = new int[] { 1, 10, 10, 1 }; //1 input and 1 output
+private List<NeuralNetwork> nets;
+private bool leftMouseDown = false;
+public List<ForceToMove> TrainList = null;
+
+void Timer()
+{
+    isTraning = false;
+}
+
+
+void Update()
+{
+    if (isTraning == false)
+    {
+        if (generationNumber == 0)
+        {
+            InitTrainNeuralNetworks();
+        }
+        else
+        {
+            nets.Sort();
+            for (int i = 0; i < populationSize; i++)
+            {
+                nets[i] = new NeuralNetwork(nets[i]);
+                nets[i].Mutate();
+
+                nets[i] = new NeuralNetwork(nets[i]); //too lazy to write a reset neuron matrix values method....so just going to make a deepcopy
+            }
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                nets[i].SetFitness(0f);
+            }
+        }
+
+
+        generationNumber++;
+
+        isTraning = true;
+        Invoke("Timer", Interval);
+        CreateTrain();
+        DestroyTrain();
+    }
+
+    /*
+    if (Input.GetMouseButtonDown(0))
+    {
+        leftMouseDown = true;
+    }
+    else if (Input.GetMouseButtonUp(0))
+    {
+        leftMouseDown = false;
+    }
+
+    if (leftMouseDown == true)
+    {
+        
+    }
+    */
+
+}
+
+    
+
+    private void DestroyTrain()
+    {
+        for (int i = 0; i < TrainList.Count; i++)
+        {
+            GameObject.Destroy(TrainList[i].gameObject);
+        }
+    }
+
+private void CreateTrain()
+{
+    if (TrainList != null)
+    {
+            DestroyTrain();
+    }
+
+    TrainList = new List<ForceToMove>();
+
+    for (int i = 0; i < populationSize; i++)
+    {
+        ForceToMove train = ((GameObject)Instantiate(TrainPrefab, new Vector3(0f,5f,0f), TrainPrefab.transform.rotation)).GetComponent<ForceToMove>();
+        train.Init(nets[i]);
+        TrainList.Add(train);
+    }
+
+}
+
+void InitTrainNeuralNetworks()
+{
+    //population must be even
+    if (populationSize % 2 != 0)
+    {
+            populationSize = 20;
+    }
+
+    nets = new List<NeuralNetwork>();
+
+
+    for (int i = 0; i < populationSize; i++)
+    {
+        NeuralNetwork net = new NeuralNetwork(layers);
+        net.Mutate();
+        nets.Add(net);
+    }
+
+        
+}
+
+}
